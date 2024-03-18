3b141336ba78c127eaf914fd31e8e978523807d1
Colin <cbarkwith@hotmail.com>
2024-03-18 12:53:13 +0000

    Improvements to index template

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -2,18 +2,22 @@
 @section('content')
     <h1 class="text-3xl font-bold">Books</h1>
     <table class="border-solid border-2 border-indigo-600">
-        <tr class="border-solid border-2 border-indigo-600">
-            <th>No</th>
-            <th>Author</th>
-            <th>Title</th>
-            <th>Publication Year</th>
+        <tr>
+            <th class="px-8 py-4">No</th>
+            <th class="px-8 py-4">Author</th>
+            <th class="px-8 py-4">Title</th>
+            <th class="px-8 py-4">Publication Year</th>
+            <th class="px-8 py-4"></th>
+            <th class="px-8 py-4"></th>
         </tr>
         @foreach ($books as $book)
-            <tr class="border-solid border- border-indigo-600">
-                <td>{{ ++$i }}</td>
-                <td>{{ $book->Author }}</td>
-                <td>{{ $book->Title }}</td>
-                <td>{{ $book->PublicationYear }}</td>
+            <tr>
+                <td class="border-solid border-2 border-indigo-600 px-8 py-4">{{ ++$i }}</td>
+                <td class="border-solid border-2 border-indigo-600 px-8 py-4">{{ $book->Author }}</td>
+                <td class="border-solid border-2 border-indigo-600 px-8 py-4">{{ $book->Title }}</td>
+                <td class="border-solid border-2 border-indigo-600 px-8 py-4">{{ $book->PublicationYear }}</td>
+                <td class="border-solid border-2 border-indigo-600 px-8 py-4"><a href="http://localhost:8000/book/{{ $i }}">Edit</a></td>
+                <td class="border-solid border-2 border-indigo-600 px-8 py-4">{{ $book->PublicationYear }}</td>
             </tr>
         @endforeach
     </table>
